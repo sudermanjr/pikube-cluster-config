22ddef7f7c8281584b7778b8d9d9010610cf00f5
Andrew Suderman <andrew@sudermanjr.com>
2018-01-20 15:56:06 -0700

    All features working now

--- gen_configs.py
+++ gen_configs.py
@@ -6,19 +6,34 @@ import string
 import secrets
 import random
 import os
+import crypt
 import pprint
 import rstr
 import netaddr
 import base64
+import logging
 
 def gen_pass():
     """
-    Generates a password between 28 and 32 chars
+    Generates a password between 35 and 45 chars
     """
-    length = random.randint(7,9)*4
+    length = random.randint(7,9)*5
     alphabet = string.ascii_letters + string.digits
     password = ''.join(secrets.choice(alphabet) for i in range(length))
     return password
+
+
+def sha512_crypt(password, salt=None, rounds=None):
+    if salt is None:
+        rand = random.SystemRandom()
+        salt = ''.join([rand.choice(string.ascii_letters + string.digits)
+                        for _ in range(8)])
+
+    prefix = '$6$'
+    if rounds is not None:
+        rounds = max(1000, min(999999999, rounds or 5000))
+        prefix += 'rounds={0}$'.format(rounds)
+    return crypt.crypt(password, prefix + salt)
 
 
 def line_prepender(filename, line):
@@ -33,21 +48,41 @@ def gen_token():
 	Builds a token from regex [a-z0-9]{6}\.[a-z0-9]{16} 
 	This token can be used for kubeadm init and join
     """
+    LOG.debug('Generating Token')
     token = rstr.xeger(r'[a-z0-9]{6}\.[a-z0-9]{16}')
+    LOG.debug("Token: {0}".format(token))
     return token
 
 
 def build_users(config):
     """ Build a user object """
     users = []
+
     # Generate the users
     for user in config['users']:
-        newuser = {'name': 'USERNAME', 'gecos': 'Hypriot Pirate', 'sudo': 'ALL=(ALL) NOPASSWD:ALL', 'shell': '/bin/bash', 'groups': 'users,docker,video', 'plain_text_passwd': gen_pass(), 'lock_passwd': False, 'ssh_pwauth': True, 'chpasswd': {'expire': False}}
+        newuser = {}
+        newuser['gecos'] = user['name']
         newuser['name'] = user['name']
+        newuser['shell'] = '/bin/bash'
+        passwd = gen_pass()
+        newuser['passwd'] = sha512 = sha512_crypt(passwd, None, 4096)
+        newuser['lock_passd'] = False
+        newuser['chpasswd'] = {'expire': False}
 
-        # Remove admin pivs from non-admins
-        if not user['admin']:
-            newuser['sudo'] = ""
+        if 'sshPublicKey' in user:
+            # Set their key and disable pw auth
+            newuser['ssh-authorized-keys:'] = user['sshPublicKey']
+            newuser['ssh_pwauth'] = False
+        else:
+            # Log the password so that we can find it later, and enable pwauth
+            LOG.info('The password for {0} will be {1}'.format(user['name'], passwd))
+            newuser['ssh_pwauth'] = True
+
+        # Give admins some stuff
+        if user['admin']:
+            newuser['sudo'] = 'ALL=(ALL) NOPASSWD:ALL'
+            newuser['groups'] = "users,docker,video"
+        else:
             newuser['groups'] = "users"
         users.append(newuser)
     return users
@@ -110,6 +145,7 @@ def build_base_commands():
 
 def build_master(config, token):
     """ Bulds the master config"""
+    LOG.debug('Building master config')
     master_config = {}
 
     # Fix the hostname
@@ -123,10 +159,7 @@ def build_master(config, token):
 
     # Add the commands to init the master
     master_config['runcmd'].append(r'kubeadm init --token {0}'.format(token))
-    master_config['runcmd'].append(r'mkdir /home/asuderma/.kube')
-    master_config['runcmd'].append(r'cp /etc/kubernetes/admin.conf /home/asuderma/.kube/config')
-    master_config['runcmd'].append(r'chown -R asuderma /home/asuderma/.kube')
-    
+    master_config['runcmd'].append(r'export KUBECONFIG=/etc/kubernetes/admin.conf') 
     if config['kubeadm']['network'] == 'weavenet':
         master_config['runcmd'].append(r'export kubever=$(kubectl version | base64 | tr -d "\n")')
         master_config['runcmd'].append(r'kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"')
@@ -148,7 +181,8 @@ def build_master(config, token):
 
 def build_node( config, token, node):
     """ Builds a node config """
-    node_config = []
+    LOG.debug('Building Node Configs')
+    node_config = {}
 
     #Set hostname
     node_config['hostname'] = "{0}-node{1}".format(config['host-prefix'], node)
@@ -160,10 +194,10 @@ def build_node( config, token, node):
     node_config['runcmd'] = build_base_commands()
 
     # Join the cluster
-    master_config['runcmd'].append(r'kubeadm join --token {0} --discovery-token-unsafe-skip-ca-verification'.format(token))
-
+    node_config['runcmd'].append(r'kubeadm join --token {0} --discovery-token-unsafe-skip-ca-verification'.format(token))
+    
     # Add network config
-    network_config['write_files'] = build_network_config(config, node)
+    node_config['write_files'] = build_network_config(config, node)
     
     #Write the file
     filename = "{0}-node{1}.yaml".format(config['host-prefix'], node)
@@ -172,10 +206,25 @@ def build_node( config, token, node):
     line_prepender(filename, "#cloud-config")
     return None
 
+
+def build_all_nodes(config, token):
+    count = 1
+    while (count <= config['node_count']):
+        LOG.debug('Generating node: {0}'.format(count))
+        build_node(config, token, count)
+
+        count += 1
+    return None
+
 if __name__ == "__main__":
+    # setup loggig
+    logging.basicConfig( format="%(asctime)s %(levelname)7s  %(funcName)s %(message)s")
+    LOG = logging.getLogger("pikube")
+    LOG.setLevel(logging.INFO)
+    PP = pprint.PrettyPrinter(depth=6)
+
     # Pull in the master config as dict
     user_config = yaml.load(open("cluster_config.yaml", "r" ))
-    PP = pprint.PrettyPrinter(depth=6)
     my_token = gen_token()
     build_master(user_config, my_token)
-
+    build_all_nodes(user_config, my_token)
